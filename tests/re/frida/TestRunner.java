5ac286375392392e24d3728a9fea737ee72c58cf
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-02-01 21:38:25 +0100

    Fix dex build and improve structure

--- tests/re/frida/TestRunner.java
+++ tests/re/frida/TestRunner.java
@@ -1,6 +1,6 @@
-package re.frida.test;
+package re.frida;
 
-public class Runner {
+public class TestRunner {
 
     public static void main(String[] args) {
         System.out.println("Hello Frida");
