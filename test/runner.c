3be1011954cf0e5638d84a1d7b5be465edf095bd
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-02-03 03:30:36 +0100

    Implement Script teardown

--- test/runner.c
+++ test/runner.c
@@ -4,6 +4,7 @@
 #include <jni.h>
 
 typedef struct _CreateScriptOperation CreateScriptOperation;
+typedef struct _DestroyScriptOperation DestroyScriptOperation;
 
 struct _CreateScriptOperation
 {
@@ -18,15 +19,30 @@ struct _CreateScriptOperation
   GCond cond;
 };
 
+struct _DestroyScriptOperation
+{
+  GumScript * script;
+
+  gboolean done;
+
+  GMutex lock;
+  GCond cond;
+};
+
 static void frida_java_init_vm (JavaVM ** vm, JNIEnv ** env);
 static void frida_java_register_script_api (JNIEnv * env);
+static JNIEnv * frida_java_get_env (void);
 
 static jlong re_frida_script_create (JNIEnv * env, jobject self, jstring source_code);
 static gboolean create_script_on_js_thread (gpointer user_data);
 static void on_create_ready (GObject * source_object, GAsyncResult * result, gpointer user_data);
 static void on_load_ready (GObject * source_object, GAsyncResult * result, gpointer user_data);
 static void re_frida_script_destroy (JNIEnv * env, jobject self, jlong handle);
+static gboolean destroy_script_on_js_thread (gpointer user_data);
+static void on_unload_ready (GObject * source_object, GAsyncResult * result, gpointer user_data);
 static void re_frida_script_on_message (GumScript * script, const gchar * message, GBytes * data, gpointer user_data);
+
+static void destroy_weak_ref (jweak ref);
 
 static const JNINativeMethod re_frida_script_methods[] =
 {
@@ -165,6 +181,24 @@ frida_java_register_script_api (JNIEnv * env)
   (*env)->DeleteLocalRef (env, script);
 }
 
+static JNIEnv *
+frida_java_get_env (void)
+{
+  jint result;
+  JNIEnv * env;
+
+  result = (*java_vm)->GetEnv (java_vm, (void **) &env, JNI_VERSION_1_6);
+  if (result != JNI_OK)
+  {
+    g_assert_cmpint (result, ==, JNI_EDETACHED);
+
+    result = (*java_vm)->AttachCurrentThread (java_vm, &env, NULL);
+    g_assert_cmpint (result, ==, JNI_OK);
+  }
+
+  return env;
+}
+
 static jlong
 re_frida_script_create (JNIEnv * env, jobject self, jstring source_code)
 {
@@ -241,7 +275,7 @@ on_create_ready (GObject * source_object, GAsyncResult * result, gpointer user_d
     return;
   }
 
-  gum_script_set_message_handler (op->script, re_frida_script_on_message, op->wrapper, NULL);
+  gum_script_set_message_handler (op->script, re_frida_script_on_message, op->wrapper, destroy_weak_ref);
 
   gum_script_load (op->script, NULL, on_load_ready, op);
 }
@@ -259,25 +293,70 @@ on_load_ready (GObject * source_object, GAsyncResult * result, gpointer user_dat
 static void
 re_frida_script_destroy (JNIEnv * env, jobject self, jlong handle)
 {
-  g_print ("TODO: destroy()\n");
+  DestroyScriptOperation op;
+  GSource * idle_source;
+
+  op.script = GUM_SCRIPT (handle);
+
+  op.done = FALSE;
+
+  g_mutex_init (&op.lock);
+  g_cond_init (&op.cond);
+
+  idle_source = g_idle_source_new ();
+  g_source_set_callback (idle_source, destroy_script_on_js_thread, &op, NULL);
+  g_source_attach (idle_source, js_context);
+  g_source_unref (idle_source);
+
+  g_mutex_lock (&op.lock);
+  while (!op.done)
+    g_cond_wait (&op.cond, &op.lock);
+  g_mutex_unlock (&op.lock);
+
+  g_cond_clear (&op.cond);
+  g_mutex_clear (&op.lock);
+}
+
+static void
+destroy_script_operation_notify_complete (DestroyScriptOperation * self)
+{
+  g_mutex_lock (&self->lock);
+  self->done = TRUE;
+  g_cond_signal (&self->cond);
+  g_mutex_unlock (&self->lock);
+}
+
+static gboolean
+destroy_script_on_js_thread (gpointer user_data)
+{
+  DestroyScriptOperation * op = user_data;
+
+  gum_script_unload (op->script, NULL, on_unload_ready, op);
+
+  return FALSE;
+}
+
+static void
+on_unload_ready (GObject * source_object, GAsyncResult * result, gpointer user_data)
+{
+  DestroyScriptOperation * op = user_data;
+
+  gum_script_unload_finish (op->script, result);
+
+  g_object_unref (op->script);
+  op->script = NULL;
+
+  destroy_script_operation_notify_complete (op);
 }
 
 static void
 re_frida_script_on_message (GumScript * script, const gchar * message, GBytes * data, gpointer user_data)
 {
-  jweak weak_wrapper = user_data;
-  jint result;
   JNIEnv * env;
+  jweak weak_wrapper = user_data;
   jobject wrapper;
 
-  result = (*java_vm)->GetEnv (java_vm, (void **) &env, JNI_VERSION_1_6);
-  if (result != JNI_OK)
-  {
-    g_assert_cmpint (result, ==, JNI_EDETACHED);
-
-    result = (*java_vm)->AttachCurrentThread (java_vm, &env, NULL);
-    g_assert_cmpint (result, ==, JNI_OK);
-  }
+  env = frida_java_get_env ();
 
   (*env)->PushLocalFrame (env, 3);
 
@@ -288,6 +367,16 @@ re_frida_script_on_message (GumScript * script, const gchar * message, GBytes * 
   }
 
   (*env)->PopLocalFrame (env, NULL);
+}
+
+static void
+destroy_weak_ref (jweak ref)
+{
+  JNIEnv * env;
+
+  env = frida_java_get_env ();
+
+  (*env)->DeleteWeakGlobalRef (env, ref);
 }
 
 void
