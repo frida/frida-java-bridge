050ca4c7336eac5667043387e1a407c1e4e68645
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-02-04 01:44:13 +0100

    Bump the default API level to 21
    Most developers probably want to focus on ART these days.

--- test/config.mk
+++ test/config.mk
@@ -2,6 +2,6 @@ ANDROID_SDK_ROOT ?= $(shell echo ~/Library/Android/sdk)
 ANDROID_NDK_ROOT ?= /usr/local/opt/android-ndk-r13b
 ANDROID_ARCH ?= arm
 ANDROID_ABI ?= armeabi-v7a
-ANDROID_API_LEVEL ?= 14
+ANDROID_API_LEVEL ?= 21
 ANDROID_LIBDIR ?= /system/lib
 DEBUG_PORT ?= 5042
