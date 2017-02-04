90d69d699d7dc66b2d6a7417c67cb7051bb3bdd2
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-02-04 01:37:07 +0100

    Move configuration to config.mk
    Also lower the default API level to 14.

---
+++ test/config.mk
@@ -0,0 +1,7 @@
+ANDROID_SDK_ROOT ?= $(shell echo ~/Library/Android/sdk)
+ANDROID_NDK_ROOT ?= /usr/local/opt/android-ndk-r13b
+ANDROID_ARCH ?= arm
+ANDROID_ABI ?= armeabi-v7a
+ANDROID_API_LEVEL ?= 14
+ANDROID_LIBDIR ?= /system/lib
+DEBUG_PORT ?= 5042
