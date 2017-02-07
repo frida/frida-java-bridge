f257c975b48b6050b7e304f860da9c0ae09bc3f2
J Van Dyke <giantpune@gmail.com>
2017-02-07 14:01:07 -0500

    Add some more tests (#14)

--- test/re/frida/MethodTest.java
+++ test/re/frida/MethodTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
+import static org.junit.Assert.assertNull;
 import org.junit.rules.ExpectedException;
 
 import javax.crypto.Cipher;
 import java.io.IOException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MethodTest {
     @Rule
@@ -57,6 +60,165 @@ public class MethodTest {
     }
 
     @Test
+    public void interfaceCannotBeInstantiated() {
+        loadScript("var X509TrustManager = Java.use('javax.net.ssl.X509TrustManager');" +
+                "try {" +
+                "  var tm = X509TrustManager.$new();" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('couldnt create trustmanager');" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    @Test
+    public void genericReturnJavaLangClass() {
+        loadScript("var C = Java.use('java.lang.Class');" +
+                "try {" +
+                "  var method1 = C.forName.overload('java.lang.String');" +
+                "  method1.implementation = function (s) {" +
+                "    return method1.call(this, s);" +
+                "  };" +
+                "  var d = C.forName('re.frida.MethodTest');" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('class.forName failed. ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    @Test
+    public void genericReturnBadger() {
+        loadScript("var C = Java.use('re.frida.Badger');" +
+                "try {" +
+                "  var method1 = C.forName;" +
+                "  method1.implementation = function () {" +
+                "    return method1.call(this);" +
+                "  };" +
+                "  var d = C.forName();" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('forName failed. ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    // this one still just producing 
+    // Error: access violation accessing 0xf2b295fe
+    @Test
+    public void nativeReturnGenericVmStack() { 
+        loadScript(
+                "try {" +
+                "  var C = Java.use('dalvik.system.VMStack');" +
+                "  var method1 = C.getStackClass2;" +
+                "  method1.implementation = function () {" +
+                "    return method1.call(this);" +
+                "  };" +
+                "  var stack = C.getStackClass2();" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('nativeReturnGeneric: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    // this one still just producing 
+    // Error: access violation accessing 0x2133c66a
+    @Test
+    public void nativeReturnGenericBadgerWrapperAroundJavaLangClass() { 
+        loadScript(
+                "try {" +
+                "  var C = Java.use('re.frida.Badger');" +
+                "  var method1 = C.forNameYo;" +
+                "  method1.implementation = function () {" +
+                "    return method1.call(this);" +
+                "  };" +
+                "  var test = C.forNameYo('re.frida.Badger', false, null);" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('nativeReturnGeneric: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    // this one was just hanging indefinitely during the test, but in an actual app, it was crashing
+    //! either one of those is bad.
+    //@Test
+    public void methodInvoke() {
+        loadScript("var C = Java.use('java.lang.reflect.Method');" +
+                "var C2 = Java.use('java.lang.Class');" +
+                "try {" +
+                // hook the original
+                "  var method1 = C.invoke;" +
+                "  method1.implementation = function () {" +
+                "    return method1.apply(this, arguments);" +
+                "  };" +
+                
+                // now call it and see what happens
+                "  var cl = C2.forName('re.frida.Badger');" +
+                "  var method2 = cl.getMethod('returnZero', 'int');" +
+                "  var ret = method2.invoke();" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('Method.invoke: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    @Test
+    public void loadWorks() {
+        loadScript("var C = Java.use('java.lang.System');" +
+                "try {" +
+                "  var method1 = C.load;" +
+                "  method1.implementation = function (s) {" +
+                "    return method1.call(this, s);" +
+                "  };" +
+                "  C.load('/system/lib/libc.so');" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('System.load: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    @Test
+    public void runtimeLoadLibrary() {        
+        loadScript("var C = Java.use('java.lang.Runtime');" +
+                "try {" +
+                "  var method1 = C.loadLibrary.overload('java.lang.String');" +
+                "  method1.implementation = function (s) {" +
+                "    return method1.call(this, s);" +
+                "  };" +
+                
+                // now look up the function again and call it
+                "  var now = C.loadLibrary.overload('java.lang.String');" +
+                "  now.call(C, '/system/lib/libc.so');" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('Runtime.loadLibrary: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    //@Test
+    public void constructorReturnsCorrectType() {
+        loadScript("var C = Java.use('javax.crypto.spec.SecretKeySpec');" +
+                "try {" +
+                "  var method1 = C.$init.overload('[B', 'java.lang.String');" +
+                "  method1.implementation = function (a, b) {" +
+                "    return method1.call(this, a, b);" +
+                "  };" +
+                
+                // now look up the function again and call it
+                "  var testConstructor = C.$new([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 'AES');" +
+                "  send('ok');" +
+                "} catch (e) {" + 
+                "  send('SecretKeySpec: ' + e);" + 
+                "}");
+        assertEquals("ok", script.getNextMessage());
+    }
+
+    @Test
     public void staticFieldCanBeRead() {
         loadScript("var Cipher = Java.use('javax.crypto.Cipher');" +
                 "send('' + Cipher.ENCRYPT_MODE.value);");
@@ -87,5 +249,17 @@ public class MethodTest {
 class Badger {
     void die() {
         throw new IllegalStateException("Already dead");
+    }
+
+    static Class<?> forName() {
+        return Badger.class;
+    }
+
+    public static Class<?> forNameYo(String className, boolean shouldInitialize, ClassLoader classLoader) throws ClassNotFoundException {
+        return java.lang.Class.forName(className, shouldInitialize, classLoader);
+    }
+
+    public int returnZero() {
+        return 0;
     }
 }
