9dd4ed5d7c00cea28ce004d7551d1262a500f416
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-02-09 12:30:14 +0100

    Enable -Wall and -Werror

--- test/Android.mk
+++ test/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := runner
 LOCAL_SRC_FILES := runner.c dummy.cpp
 LOCAL_STATIC_LIBRARIES := frida-gumjs
-LOCAL_CFLAGS := -DFRIDA_JAVA_TESTS_DATA_DIR=\"$(FRIDA_JAVA_TESTS_DATA_DIR)\"
+LOCAL_CFLAGS := -Wall -Werror -DFRIDA_JAVA_TESTS_DATA_DIR=\"$(FRIDA_JAVA_TESTS_DATA_DIR)\"
 LOCAL_LDFLAGS := -Wl,--version-script,runner.version -Wl,--export-dynamic
 include $(BUILD_EXECUTABLE)
 
