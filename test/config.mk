e2f5e6766965639e221c2173537e50e66dd5d743
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-06-05 00:42:57 +0200

    Switch the default test configuration to arm64

--- test/config.mk
+++ test/config.mk
@@ -1,8 +1,8 @@
 ANDROID_SDK_ROOT ?= $(shell echo ~/Library/Android/sdk)
 ANDROID_NDK_ROOT ?= /usr/local/opt/android-ndk-r13b
-ANDROID_ARCH ?= arm
-ANDROID_ABI ?= armeabi-v7a
-ANDROID_API_LEVEL ?= 21
+ANDROID_ARCH ?= arm64
+ANDROID_ABI ?= arm64-v8a
+ANDROID_API_LEVEL ?= 24
 ANDROID_BINDIR ?= /system/bin
-ANDROID_LIBDIR ?= /system/lib
+ANDROID_LIBDIR ?= /system/lib64
 DEBUG_PORT ?= 5042
