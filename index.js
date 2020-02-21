058da8b84933d149e4d1910749886409f9a3d453
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-02-21 18:25:03 +0100

    Refactor the toplevel API

--- index.js
+++ index.js
@@ -17,156 +17,140 @@ const {
 
 const pointerSize = Process.pointerSize;
 
-function Runtime () {
-  let initialized = false;
-  let api = null;
-  let apiError = null;
-  let vm = null;
-  let classFactory = null;
-  let pending = [];
-  let cachedIsAppProcess = null;
+class Runtime {
+  constructor () {
+    this.classFactory = null;
+    this.ClassFactory = ClassFactory;
+    this.vm = null;
+    this.api = null;
 
-  function tryInitialize () {
-    if (initialized) {
+    this._initialized = false;
+    this._apiError = null;
+    this._pending = [];
+    this._cachedIsAppProcess = null;
+
+    this._tryInitialize();
+  }
+
+  _tryInitialize () {
+    if (this._initialized) {
       return true;
     }
 
-    if (apiError !== null) {
-      throw apiError;
+    if (this._apiError !== null) {
+      throw this._apiError;
     }
 
+    let api;
     try {
       api = getApi();
+      this.api = api;
     } catch (e) {
-      apiError = e;
+      this._apiError = e;
       throw e;
     }
-
     if (api === null) {
       return false;
     }
 
-    vm = new VM(api);
+    const vm = new VM(api);
+    this.vm = vm;
 
     ClassFactory._initialize(vm, api);
-    classFactory = new ClassFactory();
+    this.classFactory = new ClassFactory();
 
-    initialized = true;
+    this._initialized = true;
 
     return true;
   }
 
-  WeakRef.bind(Runtime, function dispose () {
-    if (api !== null) {
-      vm.perform(() => {
-        const env = vm.getEnv();
-        ClassFactory._disposeAll(env);
-        Env.dispose(env);
-      });
+  _dispose () {
+    if (this.api === null) {
+      return;
     }
-  });
 
-  Object.defineProperty(this, 'available', {
-    enumerable: true,
-    get: function () {
-      return tryInitialize();
-    }
-  });
+    const {vm} = this;
+    vm.perform(() => {
+      const env = vm.getEnv();
+      ClassFactory._disposeAll(env);
+      Env.dispose(env);
+    });
+  }
 
-  Object.defineProperty(this, 'androidVersion', {
-    enumerable: true,
-    get: function () {
-      return getAndroidVersion(classFactory);
-    }
-  });
+  get available () {
+    return this._tryInitialize();
+  }
 
-  const assertJavaApiIsAvailable = () => {
-    if (!this.available) {
-      throw new Error('Java API not available');
-    }
-  };
+  get androidVersion () {
+    return getAndroidVersion();
+  }
 
-  this.synchronized = function (obj, fn) {
+  synchronized (obj, fn) {
     let objHandle = obj.hasOwnProperty('$h') ? obj.$h : obj;
     if (!(objHandle instanceof NativePointer)) {
       throw new Error('Java.synchronized: the first argument `obj` must be either a pointer or a Java instance');
     }
 
-    const env = vm.getEnv();
+    const env = this.vm.getEnv();
     checkJniResult('VM::MonitorEnter', env.monitorEnter(objHandle));
     try {
       fn();
     } finally {
       env.monitorExit(objHandle);
     }
-  };
+  }
 
-  Object.defineProperty(this, 'enumerateLoadedClasses', {
-    enumerable: true,
-    value: function (callbacks) {
-      assertJavaApiIsAvailable();
+  enumerateLoadedClasses (callbacks) {
+    this._checkAvailable();
 
-      if (api.flavor === 'art') {
-        enumerateLoadedClassesArt(callbacks);
-      } else {
-        enumerateLoadedClassesDalvik(callbacks);
+    if (this.api.flavor === 'art') {
+      this._enumerateLoadedClassesArt(callbacks);
+    } else {
+      this._enumerateLoadedClassesDalvik(callbacks);
+    }
+  }
+
+  enumerateLoadedClassesSync () {
+    const classes = [];
+    this.enumerateLoadedClasses({
+      onMatch (c) {
+        classes.push(c);
+      },
+      onComplete () {
       }
+    });
+    return classes;
+  }
+
+  enumerateClassLoaders (callbacks) {
+    this._checkAvailable();
+
+    if (this.api.flavor === 'art') {
+      this._enumerateClassLoadersArt(callbacks);
+    } else {
+      throw new Error('Enumerating class loaders is only supported on ART');
     }
-  });
+  }
 
-  Object.defineProperty(this, 'enumerateLoadedClassesSync', {
-    enumerable: true,
-    value: function () {
-      assertJavaApiIsAvailable();
-
-      const classes = [];
-      this.enumerateLoadedClasses({
-        onMatch (c) {
-          classes.push(c);
-        },
-        onComplete () {
-        }
-      });
-      return classes;
-    }
-  });
-
-  Object.defineProperty(this, 'enumerateClassLoaders', {
-    enumerable: true,
-    value: function (callbacks) {
-      assertJavaApiIsAvailable();
-
-      if (api.flavor === 'art') {
-        enumerateClassLoadersArt(callbacks);
-      } else {
-        throw new Error('Enumerating class loaders is only supported on ART');
+  enumerateClassLoadersSync () {
+    const loaders = [];
+    this.enumerateClassLoaders({
+      onMatch (c) {
+        loaders.push(c);
+      },
+      onComplete () {
       }
-    }
-  });
+    });
+    return loaders;
+  }
 
-  Object.defineProperty(this, 'enumerateClassLoadersSync', {
-    enumerable: true,
-    value: function () {
-      assertJavaApiIsAvailable();
-
-      const loaders = [];
-      this.enumerateClassLoaders({
-        onMatch (c) {
-          loaders.push(c);
-        },
-        onComplete () {
-        }
-      });
-      return loaders;
-    }
-  });
-
-  function enumerateLoadedClassesArt (callbacks) {
+  _enumerateLoadedClassesArt (callbacks) {
+    const {vm, api} = this;
     const env = vm.getEnv();
 
     const classHandles = [];
     const addGlobalReference = api['art::JavaVMExt::AddGlobalRef'];
-    const vmHandle = api.vm;
+    const {vm: vmHandle} = api;
     withRunnableArtThread(vm, env, thread => {
       const collectClassHandles = makeArtClassVisitor(klass => {
         classHandles.push(addGlobalReference(vmHandle, thread, klass));
@@ -190,19 +174,20 @@ function Runtime () {
     callbacks.onComplete();
   }
 
-  function enumerateClassLoadersArt (callbacks) {
+  _enumerateClassLoadersArt (callbacks) {
+    const {classFactory: factory, vm, api} = this;
+    const env = vm.getEnv();
+
     const visitClassLoaders = api['art::ClassLinker::VisitClassLoaders'];
     if (visitClassLoaders === undefined) {
       throw new Error('This API is only available on Nougat and above');
     }
 
-    const env = vm.getEnv();
-
-    const ClassLoader = classFactory.use('java.lang.ClassLoader');
+    const ClassLoader = factory.use('java.lang.ClassLoader');
 
     const loaderHandles = [];
     const addGlobalReference = api['art::JavaVMExt::AddGlobalRef'];
-    const vmHandle = api.vm;
+    const {vm: vmHandle} = api;
     withRunnableArtThread(vm, env, thread => {
       const collectLoaderHandles = makeArtClassLoaderVisitor(loader => {
         loaderHandles.push(addGlobalReference(vmHandle, thread, loader));
@@ -215,7 +200,7 @@ function Runtime () {
 
     try {
       loaderHandles.forEach(handle => {
-        const loader = classFactory.cast(handle, ClassLoader);
+        const loader = factory.cast(handle, ClassLoader);
         callbacks.onMatch(loader);
       });
     } finally {
@@ -227,7 +212,9 @@ function Runtime () {
     callbacks.onComplete();
   }
 
-  function enumerateLoadedClassesDalvik (callbacks) {
+  _enumerateLoadedClassesDalvik (callbacks) {
+    const {api} = this;
+
     const HASH_TOMBSTONE = ptr('0xcbcacccd');
     const loadedClassesOffset = 172;
     const hashEntrySize = 8;
@@ -250,10 +237,11 @@ function Runtime () {
     callbacks.onComplete();
   }
 
-  this.scheduleOnMainThread = function (fn) {
-    const ActivityThread = classFactory.use('android.app.ActivityThread');
-    const Handler = classFactory.use('android.os.Handler');
-    const Looper = classFactory.use('android.os.Looper');
+  scheduleOnMainThread (fn) {
+    const {classFactory: factory} = this;
+    const ActivityThread = factory.use('android.app.ActivityThread');
+    const Handler = factory.use('android.os.Handler');
+    const Looper = factory.use('android.os.Looper');
 
     const looper = Looper.getMainLooper();
     const handler = Handler.$new.overload('android.os.Looper').call(Handler, looper);
@@ -271,77 +259,98 @@ function Runtime () {
       }
     };
     message.sendToTarget();
-  };
+  }
 
-  this.perform = function (fn) {
-    assertJavaApiIsAvailable();
+  perform (fn) {
+    this._checkAvailable();
 
-    if (!isAppProcess() || classFactory.loader !== null) {
+    if (!this._isAppProcess() || this.classFactory.loader !== null) {
       try {
-        vm.perform(fn);
+        this.vm.perform(fn);
       } catch (e) {
         setTimeout(() => { throw e; }, 0);
       }
     } else {
-      pending.push(fn);
-      if (pending.length === 1) {
-        vm.perform(() => {
-          const ActivityThread = classFactory.use('android.app.ActivityThread');
-          const app = ActivityThread.currentApplication();
-          if (app !== null) {
-            const Process = classFactory.use('android.os.Process');
-            classFactory.loader = app.getClassLoader();
-
-            if (Process.myUid() === Process.SYSTEM_UID.value) {
-              classFactory.cacheDir = '/data/system';
-            } else {
-              classFactory.cacheDir = app.getCacheDir().getCanonicalPath();
-            }
-            performPending(); // already initialized, continue
-          } else {
-            let initialized = false;
-            let hookpoint = 'early';
-
-            const handleBindApplication = ActivityThread.handleBindApplication;
-            handleBindApplication.implementation = function (data) {
-              if (data.instrumentationName.value !== null) {
-                hookpoint = 'late';
-
-                const LoadedApk = classFactory.use('android.app.LoadedApk');
-                const makeApplication = LoadedApk.makeApplication;
-                makeApplication.implementation = function (forceDefaultAppClass, instrumentation) {
-                  if (!initialized) {
-                    initialized = true;
-                    classFactory.loader = this.getClassLoader();
-                    classFactory.cacheDir = classFactory.use('java.io.File').$new(this.getDataDir() + '/cache').getCanonicalPath();
-                    performPending();
-                  }
-
-                  return makeApplication.apply(this, arguments);
-                };
-              }
-
-              handleBindApplication.apply(this, arguments);
-            };
-
-            const getPackageInfoNoCheck = ActivityThread.getPackageInfoNoCheck;
-            getPackageInfoNoCheck.implementation = function (appInfo) {
-              const apk = getPackageInfoNoCheck.apply(this, arguments);
-              if (!initialized && hookpoint === 'early') {
-                initialized = true;
-                classFactory.loader = apk.getClassLoader();
-                classFactory.cacheDir = classFactory.use('java.io.File').$new(appInfo.dataDir.value + '/cache').getCanonicalPath();
-                performPending();
-              }
-              return apk;
-            };
-          }
-        });
+      this._pending.push(fn);
+      if (this._pending.length === 1) {
+        this._performPendingWhenReady();
       }
     }
-  };
+  }
 
-  function performPending () {
+  performNow (fn) {
+    this._checkAvailable();
+
+    return this.vm.perform(() => {
+      const {classFactory: factory} = this;
+
+      if (isAppProcess() && factory.loader === null) {
+        const ActivityThread = factory.use('android.app.ActivityThread');
+        const app = ActivityThread.currentApplication();
+        if (app !== null) {
+          initFactoryFromApplication(factory, app);
+        }
+      }
+
+      return fn();
+    });
+  }
+
+  _performPendingWhenReady () {
+    this.vm.perform(() => {
+      const {classFactory: factory} = this;
+
+      const ActivityThread = factory.use('android.app.ActivityThread');
+      const app = ActivityThread.currentApplication();
+      if (app !== null) {
+        initFactoryFromApplication(factory, app);
+        this._performPending();
+        return;
+      }
+
+      const runtime = this;
+      let initialized = false;
+      let hookpoint = 'early';
+
+      const handleBindApplication = ActivityThread.handleBindApplication;
+      handleBindApplication.implementation = function (data) {
+        if (data.instrumentationName.value !== null) {
+          hookpoint = 'late';
+
+          const LoadedApk = factory.use('android.app.LoadedApk');
+          const makeApplication = LoadedApk.makeApplication;
+          makeApplication.implementation = function (forceDefaultAppClass, instrumentation) {
+            if (!initialized) {
+              initialized = true;
+              initFactoryFromLoadedApk(factory, this);
+              runtime._performPending();
+            }
+
+            return makeApplication.apply(this, arguments);
+          };
+        }
+
+        handleBindApplication.apply(this, arguments);
+      };
+
+      const getPackageInfoNoCheck = ActivityThread.getPackageInfoNoCheck;
+      getPackageInfoNoCheck.implementation = function (appInfo) {
+        const apk = getPackageInfoNoCheck.apply(this, arguments);
+
+        if (!initialized && hookpoint === 'early') {
+          initialized = true;
+          initFactoryFromLoadedApk(factory, apk);
+          runtime._performPending();
+        }
+
+        return apk;
+      };
+    });
+  }
+
+  _performPending () {
+    const {vm, _pending: pending} = this;
+
     while (pending.length > 0) {
       const fn = pending.shift();
       try {
@@ -352,108 +361,99 @@ function Runtime () {
     }
   }
 
-  this.performNow = function (fn) {
-    assertJavaApiIsAvailable();
+  use (className, options) {
+    return this.classFactory.use(className, options);
+  }
 
-    if (isAppProcess() && classFactory.loader === null) {
-      vm.perform(() => {
-        const ActivityThread = classFactory.use('android.app.ActivityThread');
-        const app = ActivityThread.currentApplication();
-        if (app !== null) {
-          classFactory.loader = app.getClassLoader();
-        }
-      });
-    }
+  openClassFile (filePath) {
+    return this.classFactory.openClassFile(filePath);
+  }
 
-    vm.perform(fn);
-  };
+  choose (specifier, callbacks) {
+    this.classFactory.choose(specifier, callbacks);
+  }
 
-  this.use = function (className, options) {
-    return classFactory.use(className, options);
-  };
+  retain (obj) {
+    return this.classFactory.retain(obj);
+  }
 
-  this.openClassFile = function (filePath) {
-    return classFactory.openClassFile(filePath);
-  };
+  cast (obj, C) {
+    return this.classFactory.cast(obj, C);
+  }
 
-  this.choose = function (specifier, callbacks) {
-    classFactory.choose(specifier, callbacks);
-  };
-
-  this.retain = function (obj) {
-    return classFactory.retain(obj);
-  };
-
-  this.cast = function (obj, C) {
-    return classFactory.cast(obj, C);
-  };
-
-  this.array = function (type, elements) {
-    return classFactory.array(type, elements);
-  };
+  array (type, elements) {
+    return this.classFactory.array(type, elements);
+  }
 
   // Reference: http://stackoverflow.com/questions/2848575/how-to-detect-ui-thread-on-android
-  this.isMainThread = function () {
-    const Looper = classFactory.use('android.os.Looper');
+  isMainThread () {
+    const Looper = this.classFactory.use('android.os.Looper');
     const mainLooper = Looper.getMainLooper();
     const myLooper = Looper.myLooper();
     if (myLooper === null) {
       return false;
     }
     return mainLooper.$isSameObject(myLooper);
-  };
+  }
 
-  this.registerClass = function (spec) {
-    return classFactory.registerClass(spec);
-  };
+  registerClass (spec) {
+    return this.classFactory.registerClass(spec);
+  }
 
-  Object.defineProperty(this, 'deoptimizeEverything', {
-    enumerable: true,
-    value: function () {
-      return deoptimizeEverything(vm, vm.getEnv());
+  deoptimizeEverything () {
+    const {vm} = this;
+    return deoptimizeEverything(vm, vm.getEnv());
+  }
+
+  _checkAvailable () {
+    if (!this.available) {
+      throw new Error('Java API not available');
     }
-  });
+  }
 
-  Object.defineProperty(this, 'vm', {
-    enumerable: false,
-    get: function () {
-      return vm;
-    }
-  });
-
-  Object.defineProperty(this, 'classFactory', {
-    enumerable: false,
-    get: function () {
-      return classFactory;
-    }
-  });
-
-  Object.defineProperty(this, 'ClassFactory', {
-    enumerable: false,
-    value: ClassFactory
-  });
-
-  function isAppProcess () {
-    if (cachedIsAppProcess === null) {
-      const readlink = new NativeFunction(Module.findExportByName(null, 'readlink'), 'pointer', ['pointer', 'pointer', 'pointer'], {
+  _isAppProcess () {
+    let result = this._cachedIsAppProcess;
+    if (result === null) {
+      const readlink = new NativeFunction(Module.getExportByName(null, 'readlink'), 'pointer', ['pointer', 'pointer', 'pointer'], {
         exceptions: 'propagate'
       });
+
       const pathname = Memory.allocUtf8String('/proc/self/exe');
       const bufferSize = 1024;
       const buffer = Memory.alloc(bufferSize);
+
       const size = readlink(pathname, buffer, ptr(bufferSize)).toInt32();
       if (size !== -1) {
         const exe = buffer.readUtf8String(size);
-        cachedIsAppProcess = [/^\/system\/bin\/app_process/.test(exe)];
+        result = /^\/system\/bin\/app_process/.test(exe);
       } else {
-        cachedIsAppProcess = [true];
+        result = true;
       }
+
+      this._cachedIsAppProcess = result;
     }
 
-    return cachedIsAppProcess[0];
+    return result;
   }
-
-  tryInitialize();
 }
 
-module.exports = new Runtime();
+function initFactoryFromApplication (factory, app) {
+  const Process = factory.use('android.os.Process');
+
+  factory.loader = app.getClassLoader();
+  if (Process.myUid() === Process.SYSTEM_UID.value) {
+    factory.cacheDir = '/data/system';
+  } else {
+    factory.cacheDir = app.getCacheDir().getCanonicalPath();
+  }
+}
+
+function initFactoryFromLoadedApk (factory, apk) {
+  factory.loader = apk.getClassLoader();
+  factory.cacheDir = factory.use('java.io.File').$new(apk.getDataDir() + '/cache').getCanonicalPath();
+}
+
+const runtime = new Runtime();
+WeakRef.bind(runtime, () => { runtime._dispose(); });
+
+module.exports = runtime;
