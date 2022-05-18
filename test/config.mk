aa704e0a636fe3e4c8202ec04e923e419b5635eb
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-05-18 15:47:59 +0200

    test: Fix crash on teardown on recent Android versions
    Where we would end up loading libart.so twice.

--- test/config.mk
+++ test/config.mk
@@ -5,5 +5,6 @@ ANDROID_ABI ?= arm64-v8a
 ANDROID_API_LEVEL ?= 30
 ANDROID_BINDIR ?= /system/bin
 ANDROID_LIBDIR ?= /system/lib64
+ANDROID_VM ?= libart.so
 APEX_LIBDIRS ?= /apex/com.android.runtime/$(shell basename $(ANDROID_LIBDIR)):/apex/com.android.art/$(shell basename $(ANDROID_LIBDIR))
 DEBUG_PORT ?= 5042
