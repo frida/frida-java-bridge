0e0aa7858a07bc6b0f8747a5c53bac4a3f865717
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-06-17 18:18:49 +0200

    android: Add `methodFlags` to Java.backtrace() frames
    And expose constants for the bits defined by spec.

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ const jsizeSize = 4;
 const pointerSize = Process.pointerSize;
 
 class Runtime {
+  ACC_PUBLIC       = 0x0001;
+  ACC_PRIVATE      = 0x0002;
+  ACC_PROTECTED    = 0x0004;
+  ACC_STATIC       = 0x0008;
+  ACC_FINAL        = 0x0010;
+  ACC_SYNCHRONIZED = 0x0020;
+  ACC_BRIDGE       = 0x0040;
+  ACC_VARARGS      = 0x0080;
+  ACC_NATIVE       = 0x0100;
+  ACC_ABSTRACT     = 0x0400;
+  ACC_STRICT       = 0x0800;
+  ACC_SYNTHETIC    = 0x1000;
+
   constructor () {
     this.classFactory = null;
     this.ClassFactory = ClassFactory;
